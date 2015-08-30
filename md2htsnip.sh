5d4ce2c5a155cb4e12c0dba42905603d8dc9cf4d
markus <markus@namaniatsea.org>
2015-08-30 14:08:12 +0200

    add ### for centering by md2htsnip

--- md2htsnip.sh
+++ md2htsnip.sh
@@ -18,15 +18,19 @@ fi
 
 css_basic=$2
 css_center=center-par-tb-space
-center_pattern="\&lt;\&lt;\&lt;[ ]*\&gt;\&gt;\&gt;"   # paragraphs matching this
-                                                      # pattern will be styled
-                                                      # with css_center
+# paragraphs matching this pattern will be styled with css_center
+center_pattern1="\&lt;\&lt;\&lt;[ ]*\&gt;\&gt;\&gt;"
+center_pattern2="\\#[ ]*\\#[ ]*\\#"
+
+
 
 mdf=$1
 pandoc -f markdown -t html $mdf | \
     # replace line breaks by paragraph breaks
     sed "s/<br[ ]*\/>/<\/p>\n<p>/g" | \
     # tag <<<>>> as center-par
-    sed "s/<p>[ ]*\($center_pattern[^<]*\)<\/p>/<p class=\"$css_center\">\1<\/p>/g" | \
+    sed "s/<p>[ ]*\($center_pattern1[^<]*\)<\/p>/<p class=\"$css_center\">\1<\/p>/g" | \
+    # tag ### as center-par
+    sed "s/<p>[ ]*\($center_pattern2[^<]*\)<\/p>/<p class=\"$css_center\">\1<\/p>/g" | \
     # tag everything else as basic indented paragraph
     sed "s/<p>/<p class=\"$css_basic\">/g"
