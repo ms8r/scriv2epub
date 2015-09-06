fde02df8c0b5b13cde59d7ba461a8cb103b681b1
markus <markus@namaniatsea.org>
2015-09-06 18:34:01 +0200

    rtf2md fix

--- rtf2md.sh
+++ rtf2md.sh
@@ -2,11 +2,11 @@
 #
 #   rtf2md: convert RTF file to Markdown, using unrtf and pandoc
 #
-#   usage:  rtf2md <RTF inpit file> <Markdown output file>
+#   usage:  rtf2md <RTF input file> <Markdown output file>
 #
 unrtf --html $1 | \
-    pandoc --normalize -f html -t markdown \
-    | sed 's/\\$/\n/g' \
+    pandoc --normalize -f html -t markdown | \
+    sed 's/\\$/\n/g' | \
     # insert space after smushed italics '*'
-    | sed 's/\([\.,;:\!\?…*]\)\*\([a-zA-Z0-9“]\)/\1 \*\2/g' \
-    | pandoc --no-wrap -f markdown -t markdown  > $2
+    sed 's/\([\.,;:\!\?…*]\)\*\([a-zA-Z0-9“]\)/\1 \*\2/g' | \
+    pandoc --no-wrap -f markdown -t markdown  > $2
