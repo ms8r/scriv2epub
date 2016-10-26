2aef3a53daf5fe9998bae0f1b03f272b497a3388
mschweit66 <mschweit66@gmail.com>
2016-10-26 11:09:17 +0000

    add fix for untrf hyperlink bug

--- ipub/rtf2md.sh
+++ ipub/rtf2md.sh
@@ -5,10 +5,12 @@
 #   usage:  rtf2md <RTF input file> <Markdown output file>
 #
 unrtf --html $1 | \
-    pandoc --normalize --smart --no-wrap -f html -t markdown | \
+    # remove duplicate " around urls in hyperlinks
+    sed 's/href=""\([^"]*\)""/href="\1"/' | \
+    pandoc --normalize --smart --wrap=none -f html -t markdown | \
     # remove backslash inserted by unrtf at end of each paragraph
     sed 's/\\$/\n/g' | \
-    # remove any left oer" spans
+    # remove any left over spans
     sed 's/<\/\?span[^>]*>//g' | \
     # insert space after smushed italics '*'
     sed 's/\([\.,;:\!\?…*]\)\*\([a-zA-Z0-9“]\)/\1 \*\2/g' > $2
