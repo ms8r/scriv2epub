054105235608aaa914450f04ac52ab7c068e52c7
markus <markus@namaniatsea.org>
2017-06-04 17:26:24 +0200

    change rtf2md from unrtf to libreoffice

--- ipub/rtf2md.sh
+++ ipub/rtf2md.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 #
-#   rtf2md: convert RTF file to Markdown, using unrtf and pandoc
+#   rtf2md: convert RTF file to Markdown, using libreoffice and pandoc
 #
 #   usage:  rtf2md <RTF input file> <Markdown output file>
 #
-unrtf --html $1 | \
-    # remove duplicate " around urls in hyperlinks
-    sed 's/href=""\([^"]*\)""/href="\1"/' | \
-    pandoc --normalize --smart --wrap=none -f html -t markdown | \
-    # remove backslash inserted by unrtf at end of each paragraph
-    sed 's/\\$/\n/g' | \
-    # remove any left over spans
-    sed 's/<\/\?span[^>]*>//g' | \
-    # fix italics broken onto new line
-    ipitfix | \
-    # insert space after smushed italics '*'
-    sed 's/\([\.,;:\!\?…*]\)\*\([a-zA-Z0-9“]\)/\1 \*\2/g' > $2
+
+set -e
+
+SCRIPTFILE=$(basename $0)
+
+if [ $# != "2" ]
+then
+	echo "${SCRIPTFILE}: convert doc/odt/rtf to markdown"
+	echo "usage: ${SCRIPTFILE} <input file> <markdown output file>"
+	exit 1
+fi
+
+TMPDIR=$(mktemp -d .${SCRIPTFILE%.*}.XXXXXXXX)
+
+libreoffice --convert-to html --outdir $TMPDIR $1 2> /dev/null
+INFILE=$(basename $1)
+
+sed -e 's:</\?span[^>]*>::g' \
+    -e 's:</\?font[^>]*>::g' \
+    -e 's:<p [^>]*>:<p>:g' < $TMPDIR/${INFILE%.*}.html | \
+    pandoc --normalize --smart --wrap=none -f html -t markdown > $2
+
+rm $TMPDIR/${INFILE%.*}.html
+rmdir $TMPDIR
